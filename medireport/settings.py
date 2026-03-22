7316298ccf5c61048193a5528faa5f8b72bf706d
Namrata Yadav <namratayadav9691@gmail.com>
2026-03-22 23:21:48 +0530

    added pymysql fix

--- medireport/settings.py
+++ medireport/settings.py
@@ -2,6 +2,9 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
